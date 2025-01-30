5ad7e82c18d1bc8dd9b66a9a2efd2598fc9ec35f
Rishuraj Singh <102401151+rraawww10@users.noreply.github.com>
2025-01-30 11:05:53 +0530

    Update Tour.js

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -14,8 +14,9 @@ const Tour = ({ tour, onDelete }) => {
       <h3>{name}</h3>
       <p id={`tour-item-para-rec6d6T3q5EBIdCfD`}>{isExpanded ? info : `${info.substring(0, 200)}...`}</p>
       <button id={`see-more-${id}`} onClick={toggleInfo}>
-  {isExpanded ? "See less" : "Show more"}
+  {isExpanded ? "See less" : "See more"}
 </button>
+
       <p>${price}</p>
       <button id={`delete-btn-rec6d6T3q5EBIdCfD`} onClick={() => onDelete(id)}>Delete Tour</button>
     </div>
